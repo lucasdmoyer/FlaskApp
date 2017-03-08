ab1ed0c0d8088c58a2e57312a7af078f4927da34
Lucas Moyer <Lucas Moyer>
2017-03-07 22:07:57 -0800

    tried new engine for psql

--- todb.py
+++ todb.py
@@ -1,39 +1,42 @@
-import psycopg2
- 
-import sys
- 
-db_con = None
- 
-try:
- 
-#Create a database session
- 
-	db_con = psycopg2.connect(database='catalog', user='catalog', password='password')
-	 
-	#Create a client cursor to execute commands
-	 
-	cursor = db_con.cursor()
-	 
-	cursor.execute("CREATE TABLE customers (id SERIAL PRIMARY KEY, name VARCHAR age INTEGER);")
-	 
-	#The variables placeholder must always be a %s, psycop2 will automatically convert the values to SQL literal
-	 
-	cursor.execute("INSERT INTO customers ( name, AGE) VALUES (%s, %s)",("leo", 26))
-	 
-	db_con.commit()
-	 
-	cursor.execute("SELECT * FROM customers")
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from sqlalchemy import create_engine, asc, desc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item, User
+from flask import Flask, render_template, request, redirect, jsonify
+from flask import url_for, flash
 
-	print(cursor.fetchone())
- 
-except psycopg2.DatabaseError as e: 
- 
-	print ('Error %s' % e    )
-	 
-	sys.exit(1)
- 
-finally:
- 
-	if db_con:
-	 
-		db_con.close()
+# new imports for creating anti forgery state token
+# login_session acts like a dictionary for the user to store info in
+
+from flask import session as login_session
+
+# need to recieve code sent back from the callback method
+# IMPORTS FOR THIS STEP
+# stores client Id, client secret and other OAuth 2.0 parameters
+
+from oauth2client.client import flow_from_clientsecrets
+
+# if we run into an erryor trying to exchange an authorization code
+# for an access token. This catches the error
+
+from oauth2client.client import FlowExchangeError
+from flask import make_response
+import random
+import string
+import datetime
+import httplib2
+
+# converting in memory python to json (java script object notation)
+
+import json
+
+# apache 2 HTTP library
+
+import requests
+
+engine = create_engine('postgresql://catalog:password@54.160.150.235/catalog')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
